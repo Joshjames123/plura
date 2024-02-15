a908707557339b8ffe8a08c1ca11aaeb9c4b3e38
Joshua Buerano <bueranoj@gmail.com>
2024-02-15 18:50:44 +0800

    plura WIP

---
+++ src/app/site/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../../components/site/navigation";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <Navigation />
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
